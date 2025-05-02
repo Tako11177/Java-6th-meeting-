ada6007f0daa02d2708496c8244347cee7a29d23
Tako11177 <takokorinteli7@gmail.com>
2025-05-02 17:10:42 +0400

    math 2

--- My1class.java
+++ My1class.java
@@ -1,28 +1,10 @@
 public class My1class {
     public static void main(String[] args) {
 
+        int first =5;
+        int second =10;
 
-        System.out.println(1+1);
-        System.out.println(2-1);
-        System.out.println(2*3);
-        System.out.println(4/2);
-        System.out.println(20%3); // 20-დან ნაშთის გამოთვლა. გაყოფილი 3-ზე და ნაშთი
-
-        //2
-        //1
-        //6
-        //2
-        //2
-
-
-
-        //პასუხი
-       // Tako
-        //Korinteli
-        //32
-        //A
-        //true
-        //91.5
+        System.out.println(5+10);
 
 
     }
