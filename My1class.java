fca5239f3c10b14431fa33336b7afb1f94ba8cc2
Tako11177 <takokorinteli7@gmail.com>
2025-05-02 16:11:21 +0400

    variables

---
+++ My1class.java
@@ -0,0 +1,27 @@
+public class My1class {
+    public static void main(String[] args) {
+
+        String firstName = "Tako";
+        String lastName = "Korinteli";
+        int age = 32;
+        char grade = 'A';
+        boolean isStudent = true;
+        double lastScore = 91.5;
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(age);
+        System.out.println(grade);
+        System.out.println(isStudent);
+        System.out.println(lastScore);
+
+        //პასუხი
+       //Korinteli
+        //32
+        //A
+        //true
+        //91.5
+
+
+    }
+}
